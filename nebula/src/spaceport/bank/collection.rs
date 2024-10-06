c89cbf999ed29554fe5fdc8592e73f0795853de4
Peter Le <peterminhanle@gmail.com>
2024-10-06 04:14:32 +0000

    Initial collection implementation

---
+++ nebula/src/spaceport/bank/collection.rs
@@ -0,0 +1,28 @@
+/// An unstructured list of assets for internal use by banks
+/// to manage asset distribution across a star system.  The
+/// complete collection data structure is stored in the local
+/// database.  It can be shared with other banks by definition
+/// or request.
+struct Collection {
+    // Collection ID
+    // Collection Name
+    // Hashmap <Asset ID, Option<Asset>>
+}
+
+impl Collection {
+    fn new() -> Collection {
+        Collection {  }
+    }
+
+    fn add_asset() {
+
+    }
+
+    fn remove_asset() {
+
+    }
+
+    fn destroy() {
+
+    }
+}
