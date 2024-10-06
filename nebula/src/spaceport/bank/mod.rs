5db0691b7aa2b22028caa7ae93a1f03c246146f7
Peter Le <peterminhanle@gmail.com>
2024-10-06 04:15:47 +0000

    Added bank functions

--- nebula/src/spaceport/bank/mod.rs
+++ nebula/src/spaceport/bank/mod.rs
@@ -1,8 +1,40 @@
+use ulid::Ulid;
+
+use crate::data::Asset;
+
 mod collection;
 
-use super::super::data::Asset;
-
 // Banks hold and manage assets.
-pub struct Bank<T: Asset<T>> {
-    datatype: T
+pub struct Bank {
+    
+}
+
+impl Bank {
+    pub fn new() -> Bank {
+        Bank {  }
+    }
+
+    // Local bank operations
+    /// Storage (cozodb) -> Memory
+    pub fn withdraw<T>(id: Ulid) -> T
+    where T: Asset {
+        // cozodb_get_and_build(id) -> T
+    }
+    /// Memory -> Storage (cozodb)
+    pub fn deposit<T>(asset: T)
+    where T: Asset {
+
+    }
+
+    // Bank network operations
+    /// Request an asset from an in-network bank
+    pub fn obtain<T>(id: Ulid) -> T
+    where T: Asset {
+
+    }
+    /// Send an asset to another bank
+    pub fn transfer<T>(asset: T)
+    where T: Asset {
+
+    }
 }
