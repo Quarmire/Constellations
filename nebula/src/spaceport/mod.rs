b8b136375934f79d133300e9d6c51c73fda88387
Peter Le <peterminhanle@gmail.com>
2024-10-11 06:01:08 +0000

    test code

--- nebula/src/spaceport/mod.rs
+++ nebula/src/spaceport/mod.rs
@@ -3,28 +3,32 @@ mod bank;
 
 use std::{io, str::FromStr};
 
-use bank::Bank;
+use bank::HoloBank;
 use zenoh::{self, config::ZenohId, Config};
 
-struct Spaceport {
+pub struct Spaceport {
     // Designed around a zenoh session
     // multiple spaceports on one celestial body is possible
     // use hyperrail to communicate between local spaceports
 
     // has means to discover (scout) and establish 
     // (auto config) comms with other spaceports
+
+    // Has two means of communication: fast messages or
+    // large payloads
     
 
     // certain config is stored locally such as zenoh config
     z_session: zenoh::Session,
-    bank: Bank
+    bank: HoloBank
 }
 
 impl Spaceport {
-    async fn new() -> Spaceport {
+    pub async fn new() -> Spaceport {
         let config = Spaceport::configure().unwrap();
         let z_session = zenoh::open(config).await.unwrap();
-        let bank = Bank::new();
+        let bank = HoloBank::new("./test");
+        println!("{}", bank);
 
         Spaceport {
             z_session,
@@ -34,9 +38,9 @@ impl Spaceport {
 
     fn configure() -> io::Result<Config> {
         let mut config = zenoh::Config::default();
-        config.set_id(ZenohId::from_str("221b72df20924c15b8794c6bdb471150").unwrap());
+        config.set_id(ZenohId::from_str("221b72df20924c15b8794c6bdb471150").unwrap()).unwrap();
         config.connect.endpoints.set(
-            ["tcp/10.10.10.10:7447", "tcp/11.11.11.11:7447"].iter().map(|s|s.parse().unwrap()).collect());
+            ["tcp/10.10.10.10:7447", "tcp/11.11.11.11:7447"].iter().map(|s|s.parse().unwrap()).collect()).unwrap();
         Ok(config)
     }
 }
